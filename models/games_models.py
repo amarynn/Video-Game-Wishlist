2d93f3f37d415b39df0dadd6b4f8e74765f93145
leo <lmach106@outlook.com>
2023-05-12 15:24:41 +0930

    Updated search method to provide more accurate results

--- models/games_models.py
+++ models/games_models.py
@@ -8,7 +8,7 @@ def wishlist_items(user_id):
 
 def search_games(search_term):
     search_parameter = f"%{search_term}%"
-    return sql("SELECT * FROM games WHERE name LIKE (%s)", [search_parameter])
+    return sql("SELECT * FROM games WHERE UPPER(name) LIKE UPPER(%s)", [search_parameter])
 
 def add_to_wishlist(game_id, user_id):
     sql("INSERT INTO user_wishlist(user_id, game_id) VALUES(%s, %s) RETURNING*", [user_id, game_id])
